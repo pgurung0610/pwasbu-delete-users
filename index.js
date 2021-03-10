f12ca1f4ad9bacef5c9e05d9b9caf75236262f34
Pratik Gurung <pratik.gurung@stonybrook.edu>
2021-03-09 21:06:04 -0500

    Fix parsing of private key as environment variable

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ admin.initializeApp( {
         "type": process.env["PWASBU_USER_DELETION_TYPE"],
         "project_id": process.env["PWASBU_USER_DELETION_PROJECT_ID"],
         "private_key_id": process.env["PWASBU_USER_DELETION_PRIVATE_KEY_ID"],
-        "private_key": process.env["PWASBU_USER_DELETION_PRIVATE_KEY"],
+        "private_key": process.env["PWASBU_USER_DELETION_PRIVATE_KEY"].replace(/\\n/g, '\n'),
         "client_email": process.env["PWASBU_USER_DELETION_CLIENT_EMAIL"],
         "client_id": process.env["PWASBU_USER_DELETION_CLIENT_ID"],
         "auth_uri": process.env["PWASBU_USER_DELETION_AUTH_URI"],
